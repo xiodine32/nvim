296caed2071136257c5c1772406c0381753c73e0
Rares Neagu <xiodine32@users.noreply.github.com>
2023-05-31 13:50:18 +0300

    fix treesitter for angular

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,15 +1,16 @@
 return {
-    {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
-        dependencies = {
-            {'neovim/nvim-lspconfig'},
-            {'williamboman/mason.nvim', build = function() pcall(vim.cmd, 'MasonUpdate') end },
-            {'williamboman/mason-lspconfig.nvim'}, 
-            {'hrsh7th/nvim-cmp'},   
-            {'hrsh7th/cmp-nvim-lsp'}, 
-            {'L3MON4D3/LuaSnip'},    
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    dependencies = {
+        { 'neovim/nvim-lspconfig' },
+        {
+            'williamboman/mason.nvim',
+            build = function() pcall(vim.cmd, 'MasonUpdate') end
         },
-    },
-    { "nvim-treesitter/nvim-treesitter-angular" },
+        { 'williamboman/mason-lspconfig.nvim' },
+        { 'hrsh7th/nvim-cmp' },
+        { 'hrsh7th/cmp-nvim-lsp' },
+        { 'L3MON4D3/LuaSnip' },
+        { "elgiano/nvim-treesitter-angular",  branch = "topic/jsx-fix" }
+    }
 }
