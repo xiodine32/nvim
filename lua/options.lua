03376f0509692a6ba1b0163a52766082cd590213
Rares Neagu <xiodine32@users.noreply.github.com>
2023-06-08 15:16:54 +0300

    small changes

--- lua/options.lua
+++ lua/options.lua
@@ -5,9 +5,9 @@ vim.opt.clipboard:append({ "unnamed", "unnamedplus" })
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 8
-vim.opt.softtabstop = 8
-vim.opt.shiftwidth = 8
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
@@ -16,7 +16,7 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undodir = vim.fn.expand("~/.vim/undodir")
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
