b879746de75bf1a65750960493711195a350f583
Rares Neagu <xiodine32@users.noreply.github.com>
2023-06-10 15:29:31 +0300

    Remove redundant copilot config

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -5,7 +5,7 @@ return {
     config = function()
         require("copilot").setup({
             panel = { enabled = false },
-            suggestion = { auto_trigger = false },
+            suggestion = {},
         })
     end,
 }
