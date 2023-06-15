f13ecd9e857e61b36edb39fcbd1b70e5a1047d68
Rares Neagu <xiodine32@users.noreply.github.com>
2023-06-15 18:14:07 +0300

    move nullls to mason

--- lua/plugins/masonnullls.lua
+++ lua/plugins/masonnullls.lua
@@ -2,8 +2,12 @@ return {
     "jay-babu/mason-null-ls.nvim",
     event = { "BufReadPre", "BufNewFile" },
     dependencies = {
-        "williamboman/mason.nvim",
-        "jose-elias-alvarez/null-ls.nvim",
+        { "williamboman/mason.nvim" },
+        {
+            "jose-elias-alvarez/null-ls.nvim",
+            opts = {},
+            depends = { "nvim-lua/plenary.nvim" },
+        }
     },
     opts = {
         ensure_installed = nil,
--- lua/plugins/nullls.lua
+++
@@ -1,5 +0,0 @@
-return {
-    "jose-elias-alvarez/null-ls.nvim",
-    depends = { "nvim-lua/plenary.nvim" },
-    opts = {},
-}
